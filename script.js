246884f8b2b39d8da878a8e5fc13f063812883d3
DPenf0ld <dmpenfold100@gmail.com>
2025-07-14 16:17:46 +0100

    Added text and buttons

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".btn");
 
+    buttons.forEach((btn) => {
+        btn.addEventListener('mousemove', (e) => {
+            const rect = btn.getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
 
-
+            btn.style.setProperty('--x', x + 'px');
+            btn.style.setProperty('--y', y + 'px');
+        });
+    });
 });
